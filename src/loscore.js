145c02666d0fa47b1434cda6f4f9d0a1e43408dc
heysivani <heysivani@gmail.com>
2020-09-28 15:38:40 +0900

    uniq passes all tests
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>

--- src/loscore.js
+++ src/loscore.js
@@ -9,7 +9,13 @@ class LoScore {
   |~~~~~~~~~~
   * */
   uniq(array) {
-    // YOUR CODE HERE
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+      if (!newArray.includes(array[i])) {
+        newArray.push(array[i]);
+      }
+    }
+    return newArray;
   }
 
   /**
